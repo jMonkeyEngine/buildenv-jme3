fcb739645d79fbff7f6de057fd882dc74414d4c1
Riccardo Balbo <riccardo@forkforge.net>
2019-09-25 14:40:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Build environments for jMonkeyEngine3
+
+The master branch is automatically built and deployed to https://hub.docker.com/u/jmonkeyengine
+
+### How to build locally
+
+If you want to build the images locally for test and development, you will need a bash environment 
+(eg any linux distribution with bash, macos, git|cywin|win bash on windows or windows 10 wsl) and docker.
+
+
+Build and run
+```bash
+$ buildEnv="base" # linuxArm, android
+$ bash make.sh build buildenv-jme3 $buildEnv
+$ docker run --rm -it buildenv-jme3:$buildEnv bash
+```
+
+Build and deploy to a docker registry
+```bash
+$ buildEnv="base" # linuxArm, android
+$ bash make.sh build buildenv-jme3 $buildEnv NAMESPACE
+$ build make.sh deploy buildenv-jme3 $buildEnv NAMESPACE REGISTRY_USER REGISTRY_PASSWORD REGISTRY_URL
+```
---
+++ make.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -e
+
+function build {
+  docker --version
+  cdir="$PWD"
+  
+  basename="$1"
+  version="$2"
+  namespace="$3"
+  
+  tag=""  
+  buildArgs=""
+  if [ "$namespace" != "" ];
+  then
+    tag="${namespace}/"
+    buildArgs="--build-arg NAMESPACE=${namespace}/"
+  fi
+  tag="${tag}${basename}:${version}"
+     
+
+  cd $version
+  cmd="docker build --compress $buildArgs --tag $tag ."
+  echo "$cmd"
+  eval "$cmd"
+  cd "$cdir"
+}
+
+function deploy {
+  docker --version
+  basename=$1
+  version=$2
+  namespace=$3
+  user=$4
+  key=$5
+  registry=$6
+  docker login -u $user -p $key $registry
+  docker push $namespace/$basename:$version
+}
+
+
+# Usage:
+# basename="buildenv-jme3"
+# version="android"
+# registry="..."
+# user="..."
+# password="..."
+# ./make.sh build $basename $version [$namespace]
+# ./make.sh deploy $basename $version $namespace $user $password $registry
+
+$@
+
+
